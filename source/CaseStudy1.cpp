109ce52d33a80caab0164fca86cd1fb7b72d7b59
Gabriel Oliveira <gabriel.oliveira@cern.ch>
2021-04-20 18:38:29 -0300

    :poop: Add fist case study example.

---
+++ source/CaseStudy1.cpp
@@ -0,0 +1,11 @@
+#include "../source/ListGraph.h"
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    ListGraph graph;
+    graph.loadGraphFromFilePath("./assets/grafo_1.txt");
+
+    return 0;
+}
