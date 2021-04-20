9e08575f964c8b2c66f4d667de87264cf1387ed2
Gabriel Oliveira <gabriel.oliveira@cern.ch>
2021-04-20 18:37:17 -0300

    :sparkles: Add graph implementation with matrix.

---
+++ source/MatrixGraph.cpp
@@ -0,0 +1,36 @@
+#include "MatrixGraph.h";
+
+using namespace Lib;
+using namespace std;
+
+void MatrixGraph::addVertex(int index) {
+    verticesMatrix_[index] = vector<bool>(getGraphSize());
+}
+
+void MatrixGraph::addEdge(int vertex1, int vertex2) {
+    Graph::addEdge(vertex1, vertex2);
+
+    verticesMatrix_[vertex1 - 1][vertex2 - 1] = true;
+    verticesMatrix_[vertex2 - 1][vertex1 - 1] = true;
+}
+
+int MatrixGraph::getNeighbor(int vertexIndex, int neighborId) {
+    int counter = neighborId;
+    int i = 0;
+
+    while (counter >= 0 && i < getGraphSize()) {
+        if (verticesMatrix_[vertexIndex - 1][i]) {
+            counter -= 1;
+        }
+
+        i++;
+    }
+
+    return i;
+}
+
+void MatrixGraph::setupGraphWithSize(int graphSize) {
+    Graph::setupGraphWithSize(graphSize);
+
+    verticesMatrix_ = vector<vector<bool> >(graphSize);
+}
---
+++ source/MatrixGraph.h
@@ -0,0 +1,18 @@
+#include "Graph.h"
+#include <vector>
+
+namespace Lib
+{
+    class MatrixGraph : Graph
+    {
+    public:
+        void addVertex(int index) override;
+        void addEdge(int vertex1, int vertex2) override;
+
+        int getNeighbor(int vertexIndex, int neighborId) override;
+    protected:
+        void setupGraphWithSize(int graphSize) override;
+    private:
+        vector<vector<bool> > verticesMatrix_;
+    };
+}
